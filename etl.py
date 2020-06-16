b148e18708286bf32d905ad8e485ea7ddb36e6d2
Conreaux Pierre <conreauxpierre@gmail.com>
2020-06-16 22:47:52 +0200

    Update etl.py

--- etl.py
+++ etl.py
@@ -1,19 +1,22 @@
-import configparser
-from datetime import datetime
-import os
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf, col
-from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
+from datetime import datetime # The datetime module supplies classes for manipulating dates and times
+from pyspark.sql import SparkSession # The entry point into all functionality in Spark
+from pyspark.sql.functions import udf, col # To use user define function
+from pyspark.sql.functions import hour, dayofmonth, weekofyear, month, year, dayofweek # To manipulate date
+import pyspark.sql.types # Import type
 
+# The following code come from the original Udacity framework
+# But we don't need these lines, as we'll launch our ETL directly from our EMR
+#config = configparser.ConfigParser()
+#config.read('dl.cfg')
 
-config = configparser.ConfigParser()
-config.read('dl.cfg')
-
-os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
-os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
+#os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
+#os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
 
 
 def create_spark_session():
+    '''
+    Create a Spark session
+    '''
     spark = SparkSession \
         .builder \
         .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
